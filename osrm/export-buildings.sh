3e8da3caa228fa78f3f1870db19fe1f95000cb8f
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2024-12-07 09:06:30 +0100

    Update filter buildings

--- osrm/export-buildings.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-osmium export buildings.osm.pbf -o buildings.geojson --overwrite
--- osrm/filter-buildings.sh
+++ osrm/filter-buildings.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-osmium tags-filter data/switzerland.osm.pbf "w/building=*" -o buildings.osm.pbf
+osmium extract -b 8.494271,47.333464,8.585595,47.39374 data/switzerland.osm.pbf -o zurich.osm.pbf --overwrite
+osmium tags-filter zurich.osm.pbf "w/building=*" -o buildings.osm.pbf --overwrite
+osmium export buildings.osm.pbf -o data/buildings.geojson --overwrite
+rm zurich.osm.pbf
+rm buildings.osm.pbf
